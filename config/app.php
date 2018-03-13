37b2cf159a077c6637c87ecfcc45cc3ee68dd610
thiagodemas <thiago.demas7@gmail.com>
2018-03-13 11:24:43 -0300

    Creating tokens JWT

--- config/app.php
+++ config/app.php
@@ -219,6 +219,7 @@ return [
         'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
         'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
 
+
     ],
 
 ];
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 $this->group(['prefix' => 'v1' ], function (){
 
+    $this->post('auth', 'Auth\AuthApiController@authenticate');
+
     $this->group(['middleware' => 'jwt.auth'], function (){
         $this->post('products/search', 'Api\V1\ProductController@search');
         $this->resource('products', 'Api\V1\ProductController', ['except' => ['create', 'edit']]);
